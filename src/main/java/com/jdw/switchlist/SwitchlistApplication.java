c4254f2ea9de62d05227dc944fd513268c8c9c1b
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-10-11 16:48:22 -0700

    project initialized

---
+++ src/main/java/com/jdw/switchlist/SwitchlistApplication.java
@@ -0,0 +1,15 @@
+package com.jdw.switchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchlistApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SwitchlistApplication.class, args);
+    }
+
+}
